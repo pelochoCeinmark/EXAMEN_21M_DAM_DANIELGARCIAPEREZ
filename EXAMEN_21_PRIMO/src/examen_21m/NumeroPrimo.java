4ebc9950793e3f1659faaa8422000799c59644f4
ALUMNOS <ALUMNOS@P623>
2025-05-21 11:26:47 +0200

    examen

---
+++ EXAMEN_21_PRIMO/src/examen_21m/NumeroPrimo.java
@@ -0,0 +1,13 @@
+package examen_21m;
+
+public class NumeroPrimo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Buenos días Ceinmark, soy Daniel García. \n"
+				+ "\n"
+				+ "Introduce un número para averiguar si es primo o no:");
+		
+	}
+
+}
