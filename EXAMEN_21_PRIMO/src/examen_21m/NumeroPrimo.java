0e6d55d14842caa87cf3dd3d77e4949b7bfab051
ALUMNOS <ALUMNOS@P623>
2025-05-21 11:57:15 +0200

    commit

--- EXAMEN_21_PRIMO/src/examen_21m/NumeroPrimo.java
+++ EXAMEN_21_PRIMO/src/examen_21m/NumeroPrimo.java
@@ -1,13 +1,35 @@
 package examen_21m;
 
+import java.util.Scanner;
+
 public class NumeroPrimo {
+	
+	public static boolean CalcularPrimo(int num) {
+        for (int i=2; i<=num; i++) { 
+            if (num%i == 0) {
+            	System.out.println(i + " es un divisor.");
+            	return false;
+            }
+            
+        }
+        return true;
+    }//fin calcular primo
 
 	public static void main(String[] args) {
+		
+		Scanner ent = new Scanner(System.in);
 		
 		System.out.println("Buenos días Ceinmark, soy Daniel García. \n"
 				+ "\n"
 				+ "Introduce un número para averiguar si es primo o no:");
+	
+		int num = Integer.parseInt(ent.nextLine());
 		
-	}
+		if (CalcularPrimo(num)) {
+			System.out.println("No hay divisores. Es un número primo.");
+		}else {
+			System.out.println("No es un número primo.");
+		}
+	}//fin main
 
-}
+}//fin clase
